eb514972e24269c11934ed2b773feb8990489291
ConMint <caminto94@gmail.com>
2022-09-25 15:18:08 +0100

    api

--- src/index.js
+++ src/index.js
@@ -10,22 +10,42 @@
 import '/src/style.css';
 
 const img = document.querySelector('img');
-const q = 'dogs';
+// const q = 'dogs';
 
-async function fetchNewgif() {
+// async function fetchNewgif() {
+//   try {
+//     const response = await fetch(
+//       'https://api.giphy.com/v1/gifs/translate?api_key=byqETZYtjVS3cNVVgdJ9RoZ4QJGApt50&s=' +
+//         q,
+//       { mode: 'cors' }
+//     );
+//     const gifdata = await response.json();
+
+//     img.src = gifdata.data.images.original.url;
+//   } catch (error) {
+//     console.error('There has been a problem with your fetch operation:', error);
+//     alert('Something went wrong. Try a different search term.');
+//   }
+// }
+
+// onload = fetchNewgif;
+
+async function getWeatherData() {
   try {
     const response = await fetch(
-      'https://api.giphy.com/v1/gifs/translate?api_key=byqETZYtjVS3cNVVgdJ9RoZ4QJGApt50&s=' +
-        q,
+      'http://api.openweathermap.org/data/2.5/weather?q=paris&APPID=7293fbd3f556bdc7f99dfbe674fd9cf3&units=metric',
       { mode: 'cors' }
     );
-    const gifdata = await response.json();
+    const weatherData = await response.json();
 
-    img.src = gifdata.data.images.original.url;
+    const iconUrl = weatherData.weather[0].icon;
+    console.log(iconUrl);
+    img.src = 'http://openweathermap.org/img/wn/' + iconUrl + '@2x.png';
+    console.log(weatherData);
+    console.log(weatherData.name);
   } catch (error) {
     console.error('There has been a problem with your fetch operation:', error);
-    alert('Something went wrong. Try a different search term.');
   }
 }
 
-onload = fetchNewgif;
+onload = getWeatherData;
